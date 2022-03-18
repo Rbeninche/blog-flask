3d2b3e37d882aac5a9520dced114a233f59061ee
Rbeninche <rbeninche@gmail.com>
2022-03-17 19:03:30 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Create custom Error pages
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
